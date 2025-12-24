25cfab19e44b90eb2b5ae3cd8c8e02134316616e
Mike Parkhill <mparkhill@yahoo.com>
2025-12-24 14:23:04 -0500

    clean up deleted features in composeTools

--- src/tools/composeTools.ts
+++ src/tools/composeTools.ts
@@ -5,16 +5,8 @@ import { PresentationsClient } from "../features/presentations/client.js";
 import { RegistriesClient } from "../features/registries/client.js";
 import { SchemasClient } from "../features/schemas/client.js";
 import { ProfilesClient } from "../features/profiles/client.js";
-import { WebhooksClient } from "../features/webhooks/client.js";
-import { TemplatesClient } from "../features/templates/client.js";
-import { SubaccountsClient } from "../features/subaccounts/client.js";
-import { TeamsClient } from "../features/teams/client.js";
 import { MessagingClient } from "../features/messaging/client.js";
 import { OpenIdClient } from "../features/openid/client.js";
-import { TrustRegistriesClient } from "../features/trustRegistries/client.js";
-import { DataClient } from "../features/data/client.js";
-import { KeysClient } from "../features/keys/client.js";
-import { JobsClient } from "../features/jobs/client.js";
 import { VerifyClient } from "../features/verify/client.js";
 
 import type { ToolDef, ToolHandler } from "./types.js";
@@ -26,16 +18,8 @@ import { toolDefs as presentationsDefs, getHandlers as getPresentationsHandlers 
 import { toolDefs as registriesDefs, getHandlers as getRegistriesHandlers } from "../features/registries/index.js";
 import { toolDefs as schemasDefs, getHandlers as getSchemasHandlers } from "../features/schemas/index.js";
 import { toolDefs as profilesDefs, getHandlers as getProfilesHandlers } from "../features/profiles/index.js";
-import { toolDefs as webhooksDefs, getHandlers as getWebhooksHandlers } from "../features/webhooks/index.js";
-import { toolDefs as templatesDefs, getHandlers as getTemplatesHandlers } from "../features/templates/index.js";
-import { toolDefs as subaccountsDefs, getHandlers as getSubaccountsHandlers } from "../features/subaccounts/index.js";
-import { toolDefs as teamsDefs, getHandlers as getTeamsHandlers } from "../features/teams/index.js";
 import { toolDefs as messagingDefs, getHandlers as getMessagingHandlers } from "../features/messaging/index.js";
 import { toolDefs as openidDefs, getHandlers as getOpenidHandlers } from "../features/openid/index.js";
-import { toolDefs as trustRegistriesDefs, getHandlers as getTrustRegistriesHandlers } from "../features/trustRegistries/index.js";
-import { toolDefs as dataDefs, getHandlers as getDataHandlers } from "../features/data/index.js";
-import { toolDefs as keysDefs, getHandlers as getKeysHandlers } from "../features/keys/index.js";
-import { toolDefs as jobsDefs, getHandlers as getJobsHandlers } from "../features/jobs/index.js";
 import { toolDefs as verifyDefs, getHandlers as getVerifyHandlers } from "../features/verify/index.js";
 
 export function buildToolList(): ToolDef[] {
@@ -47,16 +31,8 @@ export function buildToolList(): ToolDef[] {
     ...registriesDefs,
     ...schemasDefs,
     ...profilesDefs,
-    ...webhooksDefs,
-    ...templatesDefs,
-    ...subaccountsDefs,
-    ...teamsDefs,
     ...messagingDefs,
     ...openidDefs,
-    ...trustRegistriesDefs,
-    ...dataDefs,
-    ...keysDefs,
-    ...jobsDefs,
     ...verifyDefs,
   ];
 }
@@ -69,16 +45,8 @@ export function buildHandlerMap(clients: {
   registries: RegistriesClient;
   schemas: SchemasClient;
   profiles: ProfilesClient;
-  webhooks: WebhooksClient;
-  templates: TemplatesClient;
-  subaccounts: SubaccountsClient;
-  teams: TeamsClient;
   messaging: MessagingClient;
   openid: OpenIdClient;
-  trustRegistries: TrustRegistriesClient;
-  data: DataClient;
-  keys: KeysClient;
-  jobs: JobsClient;
   verify: VerifyClient;
 }) {
   const handlers = new Map<string, ToolHandler>();
@@ -92,16 +60,8 @@ export function buildHandlerMap(clients: {
     getRegistriesHandlers(clients.registries),
     getSchemasHandlers(clients.schemas),
     getProfilesHandlers(clients.profiles),
-    getWebhooksHandlers(clients.webhooks),
-    getTemplatesHandlers(clients.templates),
-    getSubaccountsHandlers(clients.subaccounts),
-    getTeamsHandlers(clients.teams),
     getMessagingHandlers(clients.messaging),
     getOpenidHandlers(clients.openid),
-    getTrustRegistriesHandlers(clients.trustRegistries),
-    getDataHandlers(clients.data),
-    getKeysHandlers(clients.keys),
-    getJobsHandlers(clients.jobs),
     getVerifyHandlers(clients.verify),
   ];
 
@@ -123,16 +83,8 @@ export function buildHandlerMapFromTruvera(truvera: TruveraClient) {
     registries: new RegistriesClient(truvera),
     schemas: new SchemasClient(truvera),
     profiles: new ProfilesClient(truvera),
-    webhooks: new WebhooksClient(truvera),
-    templates: new TemplatesClient(truvera),
-    subaccounts: new SubaccountsClient(truvera),
-    teams: new TeamsClient(truvera),
     messaging: new MessagingClient(truvera),
     openid: new OpenIdClient(truvera),
-    trustRegistries: new TrustRegistriesClient(truvera),
-    data: new DataClient(truvera),
-    keys: new KeysClient(truvera),
-    jobs: new JobsClient(truvera),
     verify: new VerifyClient(truvera),
   };
 
